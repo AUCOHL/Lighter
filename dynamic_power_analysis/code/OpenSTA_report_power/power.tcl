5b7965f04b74396716a13bbab1cbd19f1d30cea7
Youssef Kandil <63082375+youssefkandil@users.noreply.github.com>
2022-02-26 19:14:41 +0200

    populating_power_analysis_table

---
+++ dynamic_power_analysis/code/OpenSTA_report_power/power.tcl
@@ -0,0 +1,16 @@
+read_liberty sky130_hd.lib
+
+
+
+
+read_verilog testing_PPU/before_gl.v
+link_design LoopyGen
+report_power
+
+
+
+read_verilog testing_PPU/test_outout_gatelevel.gl.v
+link_design LoopyGen
+report_power
+
+
