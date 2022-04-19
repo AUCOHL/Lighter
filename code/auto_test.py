55b0f9fa3321ab2feb3c8faf0f57d113eb2fc16b
Youssef Kandil <63082375+youssefkandil@users.noreply.github.com>
2022-04-20 00:08:59 +0200

    code

---
+++ code/auto_test.py
@@ -0,0 +1,147 @@
+import os
+import csv
+
+dir_list = [ 
+
+#validation
+            #"AHB_SRAM",
+            #"regfile",
+            #"blake2s",
+            #"blake2s_core",
+            #"blake2s_G",
+            #"blake2s_m_select",
+            #"chacha"
+            ] 
+
+
+states=[["module", "clock gates", "cells before","cells difference", "cells after", "a211oi_1 before", "a21oi_1 before","a22o_1 before", "a22oi_1 before" ,"a211oi_1 after", "a21oi_1 after","a22o_1 after", "a22oi_1 after", "aoi/ao difference"]]
+
+
+
+for test in dir_list:
+    print(test)
+    with open("./synth.ys", "w") as f:
+        
+        f.write(
+            '''
+read_verilog designs/''' + test + '''/''' + test + '''.v
+hierarchy -check -top ''' + test + '''
+proc;
+opt;; 
+memory_collect
+memory_map
+check
+opt;; 
+synth -top ''' + test + '''
+dfflibmap -liberty lib/sky130_hd.lib 
+abc -D 1250 -liberty lib/sky130_hd.lib 
+splitnets
+opt_clean -purge
+hilomap -hicell sky130_fd_sc_hd__conb_1 HI -locell sky130_fd_sc_hd__conb_1 LO
+splitnets
+opt_clean -purge
+insbuf -buf sky130_fd_sc_hd__buf_2 A X
+dffinit
+opt;; 
+check
+write_verilog -noattr -noexpr -nohex -nodec -defparam   designs/''' + test + '''/before_gl.v
+            '''
+        )
+
+    os.system("yosys ./synth.ys" )
+
+    with open("./synth2.ys", "w") as f:
+        
+        f.write(
+            '''
+read_verilog designs/''' + test + '''/''' + test + '''6.v
+read_verilog lib/blackbox_clk_gates.v
+hierarchy -check -top ''' + test + '''
+
+proc;
+opt;; 
+memory_collect
+memory_map
+opt;;
+check
+techmap -map lib/map_file.v
+opt;; 
+opt_clean -purge
+synth -top ''' + test + '''
+dfflibmap -liberty lib/sky130_hd.lib 
+abc -D 1250 -liberty lib/sky130_hd.lib 
+splitnets
+opt_clean -purge
+hilomap -hicell sky130_fd_sc_hd__conb_1 HI -locell sky130_fd_sc_hd__conb_1 LO
+splitnets
+opt_clean -purge
+insbuf -buf sky130_fd_sc_hd__buf_2 A X
+dffinit
+opt;; 
+check
+write_verilog -noattr -noexpr -nohex -nodec -defparam   designs/''' + test + '''/after_gl.v
+            '''
+        )
+
+
+  
+    os.system("yosys ./synth2.ys" )
+    cells_before= os.popen('grep sky130_fd_sc_hd designs/'+test+'/before_gl.v | wc -l' )
+    cells_before_no = cells_before.read()
+    cells_before_no=cells_before_no[:-1]
+    cells_after= os.popen('grep sky130_fd_sc_hd designs/'+test+'/after_gl.v | wc -l' )
+    cells_after_no = cells_after.read()
+    cells_after_no=cells_after_no[:-1]
+    clk_gates=os.popen('grep dlclk designs/'+test+'/after_gl.v | wc -l' )
+    clk_gates_no = clk_gates.read()
+    clk_gates_no=clk_gates_no[:-1]
+    cell_diff= int (cells_after_no)-int (cells_before_no)
+
+
+    cell1_before= os.popen('grep sky130_fd_sc_hd__a211oi_1 designs/'+test+'/before_gl.v | wc -l' )
+    cell1_before_no = cell1_before.read()
+    cell1_before_no=cell1_before_no[:-1]
+
+    cell2_before= os.popen('grep sky130_fd_sc_hd__a21oi_1 designs/'+test+'/before_gl.v | wc -l' )
+    cell2_before_no = cell2_before.read()
+    cell2_before_no=cell2_before_no[:-1]
+
+    cell3_before= os.popen('grep sky130_fd_sc_hd__a22o_1 designs/'+test+'/before_gl.v | wc -l' )
+    cell3_before_no = cell3_before.read()
+    cell3_before_no=cell3_before_no[:-1]
+
+    cell4_before= os.popen('grep sky130_fd_sc_hd__a22oi_1 designs/'+test+'/before_gl.v | wc -l' )
+    cell4_before_no = cell4_before.read()
+    cell4_before_no=cell4_before_no[:-1]
+
+
+    cell1_after= os.popen('grep sky130_fd_sc_hd__a211oi_1 designs/'+test+'/after_gl.v | wc -l' )
+    cell1_after_no = cell1_after.read()
+    cell1_after_no=cell1_after_no[:-1]
+
+    cell2_after= os.popen('grep sky130_fd_sc_hd__a21oi_1 designs/'+test+'/after_gl.v | wc -l' )
+    cell2_after_no = cell2_after.read()
+    cell2_after_no=cell2_after_no[:-1]
+
+    cell3_after= os.popen('grep sky130_fd_sc_hd__a22o_1 designs/'+test+'/after_gl.v | wc -l' )
+    cell3_after_no = cell3_after.read()
+    cell3_after_no=cell3_after_no[:-1]
+
+    cell4_after= os.popen('grep sky130_fd_sc_hd__a22oi_1 designs/'+test+'/after_gl.v | wc -l' )
+    cell4_after_no = cell4_after.read()
+    cell4_after_no=cell4_after_no[:-1]
+    aoi_diff= int(cell1_after_no)+ int(cell2_after_no)+ int(cell3_after_no)+int(cell4_after_no) -int(cell1_before_no)- int(cell2_before_no)- int(cell3_before_no)-int(cell4_before_no)
+   
+    row= [test,clk_gates_no, cells_before_no, cells_after_no,cell_diff,
+    cell1_before_no, cell2_before_no, cell3_before_no,cell4_before_no,  
+    cell1_after_no, cell2_after_no, cell3_after_no,cell4_after_no, aoi_diff ]
+    states.append(row)
+
+
+
+
+f = open('./stats/stats_cells_file.csv', 'w')
+writer = csv.writer(f)
+for row in states:
+    writer.writerow(row)
+f.close()
---
+++ code/power_report.py
@@ -0,0 +1,265 @@
+import os
+import csv
+import sys
+from tokenize import Double
+
+import numpy as np
+
+
+
+
+
+
+states=[["module", "Internal before","Switching before","Leakage before","Total before",
+                    "Internal after","Switching after","Leakage after","Total after",
+                     "total power difference", "percentage reduction"],]
+
+states_two=[["module", "all cells power before at 0.1","flipflops power before at 0.1","flipflops power before at 1.0","total power before",
+                    "all cells power after at 0.1","flipflops power after at 0.1","flipflops power after at 0.05","total power after",
+                     "total power difference", "percentage reduction"],]
+
+
+
+states_internal=[["module","all cells Internal before at 0.1", "all cells power before at 0.1","ff Internal before at 0.1","ff power before at 0.1","ff Internal before at 1.0","ff power before at 1.0","total power before",
+                    "all cells Internal after at 0.1", "all cells power after at 0.1","ff Internal after at 0.1", "ff power after at 0.1", "ff Internal after at 0.05","ff power after at 0.05","total power after",
+                     "total power difference", "percentage reduction"],]
+
+states_switching=[["module","all cells switching before at 0.1", "all cells power before at 0.1","ff switching before at 0.1","ff power before at 0.1","ff switching before at 1.0","ff power before at 1.0","total power before",
+                    "all cells switching after at 0.1", "all cells power after at 0.1","ff switching after at 0.1", "ff power after at 0.1", "ff switching after at 0.05","ff power after at 0.05","total power after",
+                     "total power difference", "percentage reduction"],]
+
+
+states_leakage=[["module","all cells leakage before at 0.1", "all cells power before at 0.1","ff leakage before at 0.1","ff power before at 0.1","ff leakage before at 1.0","ff power before at 1.0","total power before",
+                    "all cells leakage after at 0.1", "all cells power after at 0.1","ff leakage after at 0.1", "ff power after at 0.1", "ff leakage after at 0.05","ff power after at 0.05","total power after",
+                     "total power difference", "percentage reduction"],]
+
+
+dir_list = [
+            #["blabla",               "clk",         "65.0"],
+            #["chacha",               "clk",         "25.0"],
+            #["ldpc_decoder_802_3an", "clk",         "77.0"],
+            #["ldpcenc",              "clk",         "12.9"], 
+            ##["sp_mul" ,              "clk",         "4.4"], 
+            #["PPU" ,                 "clk",         "3.5"],
+            ##["ula",                  "clk14",       "0.8"], 
+            ##["vm80a",                "pin_clk",     "3.2"], 
+            ##["xtea" ,                "clock",       "26.03"],
+            #["y_huff",               "clk",         "14.0"], 
+            #["y_quantizer",          "clk",         "1.0"], 
+            ##["zigzag",               "clk",         "0.8"], 
+            ##["zipdiv" ,              "i_clk",       "20.0"], 
+            #["y_dct" ,               "clk",         "20.0"], 
+            #["jpeg_encoder",         "clk",         "16.0"],
+            ##["aes_cipher",           "clk",         "10.0"],
+            #["sha512",               "clk",         "40.0"], 
+            #["picorv32a",            "clk",         "24.0"], 
+            #["riscv_top_151",        "clk",         "50.0"], 
+            #["genericfir",        "i_clk",           "15.0"],
+            #["NfiVe32_RF",        "HCLK",           "10.0"],
+            #["rf_64x64",        "HCLK",                "10.0"]
+            #["AHB_FLASH_CTRL"  ,    "HCLK" ,          "10.0"    ],
+            ["AHB_SRAM"        ,    "HCLK" ,          "10.0"    ],
+            #["AHB_UART_MASTER" ,    "HCLK" ,          "10.0"    ]
+            ] 
+# add generic fir
+            #"genericfir",
+#dir_list = [
+##        #   ["FF",        "clk",         "10.0"]
+#            ["y_dct" ,               "clk",         "20.0"], 
+#          ]
+def parse_ff_power(filename):
+    count=0
+    f = open(filename, "r+")
+    acc_internal=np.longdouble(0)
+    acc_switch  =np.longdouble(0)
+    acc_leakage =np.longdouble(0)
+    acc_total   =np.longdouble(0)
+    for line in f:
+        count+=1
+        if count>3:
+
+            line=line[3:]
+            line=line.split("   ")
+            #print(line)
+            acc_internal+=  np.longdouble(line[0])
+            acc_switch  +=  np.longdouble(line[1])
+            acc_leakage +=  np.longdouble(line[2])
+            x= line[3].split(" ")
+            acc_total   +=  np.longdouble(x[0])
+    power_array=[acc_internal,acc_switch,acc_leakage,acc_total]
+    #f.truncate(0)
+    f.close()
+    return power_array
+
+
+def parse_all_power(filename):
+    f = open(filename, "r+")
+    acc_internal=np.longdouble(0)
+    acc_switch  =np.longdouble(0)
+    acc_leakage =np.longdouble(0)
+    acc_total   =np.longdouble(0)
+    for line in f:
+        if "Total                  " in line:
+            x=line.split("Total                  ")
+            #print(x)
+            line=x[1].split("   ")
+            #print(line)
+            acc_internal+=  np.longdouble(line[0])
+            acc_switch  +=  np.longdouble(line[1])
+            acc_leakage +=  np.longdouble(line[2])
+            x= line[3].split(" ")
+            acc_total   +=  np.longdouble(x[0])
+    power_array=[acc_internal,acc_switch,acc_leakage,acc_total]
+    #f.truncate(0)
+    f.close()
+    return power_array
+
+
+for test in dir_list:
+    print(test)
+    with open("./power_report.tcl", "w") as f:
+        
+        f.write(
+            '''
+read_liberty lib/sky130_hd.lib
+##############################
+##############################
+puts "'''+test[0]+'''"
+set design '''+test[0]+'''
+set clk '''+test[1]+'''
+set period '''+test[2]+'''
+##############################
+##############################
+
+read_verilog designs/$design/before_gl.v
+link_design $design
+create_clock -period $period $clk
+set_power_activity -global -activity 0.1
+report_power $design >>./stats/all_dump_stats_0_1_before.txt
+report_power $design -instances [all_registers -cells] >>./stats/ff_dump_stats_0_1_before.txt
+
+set_power_activity -global -activity 1.0
+report_power $design -instances [all_registers -cells] >>./stats/ff_dump_stats_1_0_before.txt
+##############################
+##############################
+
+read_verilog designs/$design/after_gl.v
+link_design $design
+create_clock -period $period $clk
+
+#set_clock_gating_check $clk
+set_power_activity -global -activity 0.1
+report_power $design >>./stats/all_dump_stats_0_1_after.txt
+report_power $design -instances [all_registers -cells] >>./stats/ff_dump_stats_0_1_after.txt
+
+set_power_activity -global -activity 0.05
+report_power $design -instances [all_registers -cells] >>./stats/ff_dump_stats_0_05_after.txt
+exit
+##############################
+            '''
+        )
+    f.close()
+    os.system("/Users/youssef/OpenSTA/app/sta -no_splash ./power_report.tcl" )
+
+    ff_0_1_before = parse_ff_power("./stats/ff_dump_stats_0_1_before.txt")
+
+    ff_1_0_before =parse_ff_power("./stats/ff_dump_stats_1_0_before.txt")
+    ff_0_1_after =parse_ff_power("./stats/ff_dump_stats_0_1_after.txt")
+    ff_0_05_after =parse_ff_power("./stats/ff_dump_stats_0_05_after.txt")
+
+    all_0_1_before =parse_all_power("./stats/all_dump_stats_0_1_before.txt")
+    all_0_1_after =parse_all_power('./stats/all_dump_stats_0_1_after.txt')
+
+
+
+    #before_power =  list()
+    #for item1, item2, intem3 in zip(all_0_1_before, ff_0_1_before,ff_1_0_before):
+    #    item = item1 - item2 +intem3
+    #    before_power.append(item)
+
+    before_power=list()
+    before_power.append(all_0_1_before[0] - ff_0_1_before[0] + ff_1_0_before[0])
+    before_power.append(all_0_1_before[1] - ff_0_1_before[1] + ff_1_0_before[1])
+    before_power.append(all_0_1_before[2] - ff_0_1_before[2] + ff_1_0_before[2])
+    before_power.append(all_0_1_before[3] - ff_0_1_before[3] + ff_1_0_before[3])
+
+    after_power=list()
+    after_power.append(all_0_1_after[0] - ff_0_1_after[0] + ff_0_05_after[0])
+    after_power.append(all_0_1_after[1] - ff_0_1_after[1] + ff_0_05_after[1])
+    after_power.append(all_0_1_after[2] - ff_0_1_after[2] + ff_0_05_after[2])
+    after_power.append(all_0_1_after[3] - ff_0_1_after[3] + ff_0_05_after[3])
+
+    states.append(
+                  [
+                    test[0],
+                    str(before_power[0]), str(before_power[1]),str(before_power[2]),str(before_power[3]),
+                    str(after_power[0]), str(after_power[1]),str(after_power[2]),str(after_power[3]),
+                    str(before_power[3]-after_power[3]), str(((before_power[3]-after_power[3])/before_power[3])*100)+" %"
+                    ]
+                    )
+
+
+#states_two=[["module", "all cells power before at 0.1","flipflops power before at 0.1","flipflops power before at 1.0","total power before",
+#                    "all cells power after at 0.1","flipflops power after at 0.1","flipflops power after at 0.05","total power after",
+#                     "total power difference", "percentage reduction"],]
+
+    states_two.append([
+                        test[0],
+                        str(all_0_1_before[3]), str(ff_0_1_before[3]),str(ff_1_0_before[3]),str(before_power[3]),
+                        str(all_0_1_after[3]), str(ff_0_1_after[3]),str(ff_0_05_after[3]),str(after_power[3]),
+                        str(before_power[3]-after_power[3]), str(((before_power[3]-after_power[3])/before_power[3])*100)+" %"
+    ])
+
+
+
+    states_internal.append([
+                        test[0],
+                        str(all_0_1_before[0]),str(all_0_1_before[3]),str(ff_0_1_before[0]), str(ff_0_1_before[3]),str(ff_1_0_before[0]),str(ff_1_0_before[3]),str(before_power[3]),
+                        str(all_0_1_after[0]),str(all_0_1_after[3]),str(ff_0_1_after[0]), str(ff_0_1_after[3]),str(ff_0_05_after[0]),str(ff_0_05_after[3]),str(after_power[3]),
+                        str(before_power[3]-after_power[3]), str(((before_power[3]-after_power[3])/before_power[3])*100)+" %"
+    ])
+
+    states_switching.append([
+                        test[0],
+                        str(all_0_1_before[1]),str(all_0_1_before[3]),str(ff_0_1_before[1]), str(ff_0_1_before[3]),str(ff_1_0_before[1]),str(ff_1_0_before[3]),str(before_power[3]),
+                        str(all_0_1_after[1]),str(all_0_1_after[3]),str(ff_0_1_after[1]), str(ff_0_1_after[3]),str(ff_0_05_after[1]),str(ff_0_05_after[3]),str(after_power[3]),
+                        str(before_power[3]-after_power[3]), str(((before_power[3]-after_power[3])/before_power[3])*100)+" %"
+    ])
+
+    states_leakage.append([
+                        test[0],
+                        str(all_0_1_before[2]),str(all_0_1_before[3]),str(ff_0_1_before[2]), str(ff_0_1_before[3]),str(ff_1_0_before[2]),str(ff_1_0_before[3]),str(before_power[3]),
+                        str(all_0_1_after[2]),str(all_0_1_after[3]),str(ff_0_1_after[2]), str(ff_0_1_after[3]),str(ff_0_05_after[2]),str(ff_0_05_after[3]),str(after_power[3]),
+                        str(before_power[3]-after_power[3]), str(((before_power[3]-after_power[3])/before_power[3])*100)+" %"
+    ])
+
+f = open('./stats/stats_file.csv', 'w')
+writer = csv.writer(f)
+for row in states:
+    writer.writerow(row)
+f.close()
+
+f = open('./stats/stats_two_file.csv', 'w')
+writer = csv.writer(f)
+for row in states_two:
+    writer.writerow(row)
+f.close()
+
+f = open('./stats/stats_internal_file.csv', 'w')
+writer = csv.writer(f)
+for row in states_internal:
+    writer.writerow(row)
+f.close()
+
+f = open('./stats/stats_switching_file.csv', 'w')
+writer = csv.writer(f)
+for row in states_switching:
+    writer.writerow(row)
+f.close()
+
+f = open('./stats/stats_leakage_file.csv', 'w')
+writer = csv.writer(f)
+for row in states_leakage:
+    writer.writerow(row)
+f.close()
+
