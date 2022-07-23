70386465fa71e1466333bb0db05995370b93a554
Youssef Kandil <youssefkandil@aucegypt.edu>
2022-07-23 20:16:23 +0200

    reformatted code using Black linter
    Signed-off-by: Youssef Kandil <youssefkandil@aucegypt.edu>

--- report_power/report_power.py
+++ report_power/report_power.py
@@ -20,7 +20,6 @@ import csv
 import numpy as np
 
 
-
 dir_list = [
     # ["FF"      ,             "clk",         "10.0"],
     ["AHB_SRAM", "HCLK", "10.0"],
@@ -46,7 +45,6 @@ dir_list = [
     ["y_quantizer", "clk", "1.0"],
     ["zigzag", "clk", "0.8"],
 ]
-
 
 
 states = [
@@ -137,7 +135,7 @@ states_leakage = [
 benchmarks = [
     [
         "Design",
-        "# Cells before", 
+        "# Cells before",
         "# Cells after",
         "# Clock Gates",
         "# Flipflops",
@@ -171,7 +169,7 @@ def parse_cell_collection_power_report(file_Path):
     for line in f:
         count += 1
         if count > 3:
-            
+
             line = line[3:]
             line = line.split("   ")
             # print(line)
@@ -207,7 +205,6 @@ def parse_all_cells_power_report(file_Path):
     f.truncate(0)
     f.close()
     return power_array
-
 
 
 for test in dir_list:
@@ -295,86 +292,107 @@ exit
             """
         )
     f.close()
-    
-    
+
     os.system("/usr/local/OpenSTA/app/sta -no_splash ./power_report.tcl")
-    
-    
-    # first lets parse the complete power reports at 
+
+    # first lets parse the complete power reports at
     # normal activity before and after clock gating
-    
-    all_cells_normal_before = parse_all_cells_power_report("./stats/all_cells_normal_before.txt")
-    all_cells_normal_after = parse_all_cells_power_report("./stats/all_cells_normal_after.txt")
-    
-    # Now lets calculate the correct power before clock gating 
+
+    all_cells_normal_before = parse_all_cells_power_report(
+        "./stats/all_cells_normal_before.txt"
+    )
+    all_cells_normal_after = parse_all_cells_power_report(
+        "./stats/all_cells_normal_after.txt"
+    )
+
+    # Now lets calculate the correct power before clock gating
     # considering the high  activity of the flipflop cells
-    
-    
-    # parse the low and high activites of flipflop cells 
-    ff_cells_normal_before = parse_cell_collection_power_report("./stats/ff_cells_normal_before.txt")
-    ff_cells_high_before = parse_cell_collection_power_report("./stats/ff_cells_high_before.txt")
-    
-    
+
+    # parse the low and high activites of flipflop cells
+    ff_cells_normal_before = parse_cell_collection_power_report(
+        "./stats/ff_cells_normal_before.txt"
+    )
+    ff_cells_high_before = parse_cell_collection_power_report(
+        "./stats/ff_cells_high_before.txt"
+    )
+
     # Calcultating the power before clock gating using this formula
     # power_before_clock_gating = all_cells_normal_before - ff_cells_normal_before + ff_cells_high_before
-    
+
     power_before_clock_gating = list()
-    power_before_clock_gating.append(all_cells_normal_before[0] - ff_cells_normal_before[0] + ff_cells_high_before[0])
-    power_before_clock_gating.append(all_cells_normal_before[1] - ff_cells_normal_before[1] + ff_cells_high_before[1])
-    power_before_clock_gating.append(all_cells_normal_before[2] - ff_cells_normal_before[2] + ff_cells_high_before[2])
-    power_before_clock_gating.append(all_cells_normal_before[3] - ff_cells_normal_before[3] + ff_cells_high_before[3])
-    
+    power_before_clock_gating.append(
+        all_cells_normal_before[0] - ff_cells_normal_before[0] + ff_cells_high_before[0]
+    )
+    power_before_clock_gating.append(
+        all_cells_normal_before[1] - ff_cells_normal_before[1] + ff_cells_high_before[1]
+    )
+    power_before_clock_gating.append(
+        all_cells_normal_before[2] - ff_cells_normal_before[2] + ff_cells_high_before[2]
+    )
+    power_before_clock_gating.append(
+        all_cells_normal_before[3] - ff_cells_normal_before[3] + ff_cells_high_before[3]
+    )
+
     ######################################################
     ######################################################
-    
-    # Now let's calculate the power after clock gating 
-    
-    
+
+    # Now let's calculate the power after clock gating
+
     # power_after_clock_gating = all_cells_normal_after - clockgated_ff_cells_normal + clockgated_ff_cells_low - clock_gates_normal + clock_gates_high - not_gated_flipflops_normal + not_gated_flipflops_high
-    
+
     # where
-    
+
     # clockgated_ff_cells_normal = all_cg_and_ff_normal_after - clock_gates_normal
-    
+
     # clockgated_ff_cells_low = all_cg_and_ff_low_after - clock_gates_low
-    
+
     # clockgated_ff_cells_high = all_cg_and_ff_high_after - clock_gates_high
-    
+
     # not_gated_flipflops_normal = all_flipflops_normal_after - clockgated_ff_cells_normal
-    
+
     # not_gated_flipflops_high = all_flipflops_high_after - clockgated_ff_cells_high
-    
-    
-    
-    
+
     # step one
     # clockgated_ff_cells_normal = all_cg_and_ff_normal_after - clock_gates_normal
-    
+
     # get  all_cg_and_ff_normal_after
-    all_cg_and_ff_normal_after = parse_cell_collection_power_report("./stats/all_cg_and_ff_normal_after.txt")
-    
+    all_cg_and_ff_normal_after = parse_cell_collection_power_report(
+        "./stats/all_cg_and_ff_normal_after.txt"
+    )
+
     # get  clock_gates_normal
-    clock_gates_normal = parse_cell_collection_power_report("./stats/clock_gates_normal.txt")
-    
+    clock_gates_normal = parse_cell_collection_power_report(
+        "./stats/clock_gates_normal.txt"
+    )
+
     # claculate clockgated_ff_cells_normal = all_cg_and_ff_normal_after - clock_gates_normal
     clockgated_ff_cells_normal = list()
-    clockgated_ff_cells_normal.append(all_cg_and_ff_normal_after[0] - clock_gates_normal[0])
-    clockgated_ff_cells_normal.append(all_cg_and_ff_normal_after[1] - clock_gates_normal[1])
-    clockgated_ff_cells_normal.append(all_cg_and_ff_normal_after[2] - clock_gates_normal[2])
-    clockgated_ff_cells_normal.append(all_cg_and_ff_normal_after[3] - clock_gates_normal[3])
-    
+    clockgated_ff_cells_normal.append(
+        all_cg_and_ff_normal_after[0] - clock_gates_normal[0]
+    )
+    clockgated_ff_cells_normal.append(
+        all_cg_and_ff_normal_after[1] - clock_gates_normal[1]
+    )
+    clockgated_ff_cells_normal.append(
+        all_cg_and_ff_normal_after[2] - clock_gates_normal[2]
+    )
+    clockgated_ff_cells_normal.append(
+        all_cg_and_ff_normal_after[3] - clock_gates_normal[3]
+    )
+
     ####################################################################################################
-    
-    
+
     # step Two
     # clockgated_ff_cells_low = all_cg_and_ff_low_after - clock_gates_low
-    
+
     # get  all_cg_and_ff_low_after
-    all_cg_and_ff_low_after = parse_cell_collection_power_report("./stats/all_cg_and_ff_low_after.txt")
-    
+    all_cg_and_ff_low_after = parse_cell_collection_power_report(
+        "./stats/all_cg_and_ff_low_after.txt"
+    )
+
     # get  clock_gates_low
     clock_gates_low = parse_cell_collection_power_report("./stats/clock_gates_low.txt")
-    
+
     # claculate clockgated_ff_cells_low = all_cg_and_ff_low_after - clock_gates_low
     clockgated_ff_cells_low = list()
     clockgated_ff_cells_low.append(all_cg_and_ff_low_after[0] - clock_gates_low[0])
@@ -382,17 +400,20 @@ exit
     clockgated_ff_cells_low.append(all_cg_and_ff_low_after[2] - clock_gates_low[2])
     clockgated_ff_cells_low.append(all_cg_and_ff_low_after[3] - clock_gates_low[3])
     ####################################################################################################
-    
-    
+
     # step three
     # clockgated_ff_cells_high = all_cg_and_ff_high_after - clock_gates_high
-    
+
     # get  all_cg_and_ff_high_after
-    all_cg_and_ff_high_after = parse_cell_collection_power_report("./stats/all_cg_and_ff_high_after.txt")
-    
+    all_cg_and_ff_high_after = parse_cell_collection_power_report(
+        "./stats/all_cg_and_ff_high_after.txt"
+    )
+
     # get  clock_gates_high
-    clock_gates_high = parse_cell_collection_power_report("./stats/clock_gates_high.txt")
-    
+    clock_gates_high = parse_cell_collection_power_report(
+        "./stats/clock_gates_high.txt"
+    )
+
     # claculate clockgated_ff_cells_high = all_cg_and_ff_high_after - clock_gates_high
     clockgated_ff_cells_high = list()
     clockgated_ff_cells_high.append(all_cg_and_ff_high_after[0] - clock_gates_high[0])
@@ -400,53 +421,101 @@ exit
     clockgated_ff_cells_high.append(all_cg_and_ff_high_after[2] - clock_gates_high[2])
     clockgated_ff_cells_high.append(all_cg_and_ff_high_after[3] - clock_gates_high[3])
     ####################################################################################################
-    
-    
-    # step four 
+
+    # step four
     # not_gated_flipflops_normal = all_flipflops_normal_after - clockgated_ff_cells_normal
-    
+
     # get  all_flipflops_normal_after
-    all_flipflops_normal_after = parse_cell_collection_power_report("./stats/all_flipflops_normal_after.txt")
-    
+    all_flipflops_normal_after = parse_cell_collection_power_report(
+        "./stats/all_flipflops_normal_after.txt"
+    )
+
     # claculate not_gated_flipflops_normal = all_flipflops_normal_after - clockgated_ff_cells_normal
     not_gated_flipflops_normal = list()
-    not_gated_flipflops_normal.append(all_flipflops_normal_after[0] - clockgated_ff_cells_normal[0])
-    not_gated_flipflops_normal.append(all_flipflops_normal_after[1] - clockgated_ff_cells_normal[1])
-    not_gated_flipflops_normal.append(all_flipflops_normal_after[2] - clockgated_ff_cells_normal[2])
-    not_gated_flipflops_normal.append(all_flipflops_normal_after[3] - clockgated_ff_cells_normal[3])
+    not_gated_flipflops_normal.append(
+        all_flipflops_normal_after[0] - clockgated_ff_cells_normal[0]
+    )
+    not_gated_flipflops_normal.append(
+        all_flipflops_normal_after[1] - clockgated_ff_cells_normal[1]
+    )
+    not_gated_flipflops_normal.append(
+        all_flipflops_normal_after[2] - clockgated_ff_cells_normal[2]
+    )
+    not_gated_flipflops_normal.append(
+        all_flipflops_normal_after[3] - clockgated_ff_cells_normal[3]
+    )
     ####################################################################################################
-    
-    
-    # step five 
+
+    # step five
     # not_gated_flipflops_high = all_flipflops_high_after - clockgated_ff_cells_high
-    
+
     # get  all_flipflops_high_after
-    all_flipflops_high_after = parse_cell_collection_power_report("./stats/all_flipflops_high_after.txt")
-    
+    all_flipflops_high_after = parse_cell_collection_power_report(
+        "./stats/all_flipflops_high_after.txt"
+    )
+
     # claculate not_gated_flipflops_high = all_flipflops_high_after - clockgated_ff_cells_high
     not_gated_flipflops_high = list()
-    not_gated_flipflops_high.append(all_flipflops_high_after[0] - clockgated_ff_cells_high[0])
-    not_gated_flipflops_high.append(all_flipflops_high_after[1] - clockgated_ff_cells_high[1])
-    not_gated_flipflops_high.append(all_flipflops_high_after[2] - clockgated_ff_cells_high[2])
-    not_gated_flipflops_high.append(all_flipflops_high_after[3] - clockgated_ff_cells_high[3])
+    not_gated_flipflops_high.append(
+        all_flipflops_high_after[0] - clockgated_ff_cells_high[0]
+    )
+    not_gated_flipflops_high.append(
+        all_flipflops_high_after[1] - clockgated_ff_cells_high[1]
+    )
+    not_gated_flipflops_high.append(
+        all_flipflops_high_after[2] - clockgated_ff_cells_high[2]
+    )
+    not_gated_flipflops_high.append(
+        all_flipflops_high_after[3] - clockgated_ff_cells_high[3]
+    )
     ####################################################################################################
-    
-    
-    # Final step 
+
+    # Final step
     # power_after_clock_gating = all_cells_normal_after - clockgated_ff_cells_normal + clockgated_ff_cells_low - clock_gates_normal + clock_gates_high - not_gated_flipflops_normal + not_gated_flipflops_high
-    
+
     # claculate power_after_clock_gating = all_cells_normal_after - clockgated_ff_cells_normal + clockgated_ff_cells_low - clock_gates_normal + clock_gates_high - not_gated_flipflops_normal + not_gated_flipflops_high
-    
+
     power_after_clock_gating = list()
-    power_after_clock_gating.append(all_cells_normal_after[0] - clockgated_ff_cells_normal[0] + clockgated_ff_cells_low[0] - clock_gates_normal[0] + clock_gates_high[0] - not_gated_flipflops_normal[0] + not_gated_flipflops_high[0])
-    power_after_clock_gating.append(all_cells_normal_after[1] - clockgated_ff_cells_normal[1] + clockgated_ff_cells_low[1] - clock_gates_normal[1] + clock_gates_high[1] - not_gated_flipflops_normal[1] + not_gated_flipflops_high[1])
-    power_after_clock_gating.append(all_cells_normal_after[2] - clockgated_ff_cells_normal[2] + clockgated_ff_cells_low[2] - clock_gates_normal[2] + clock_gates_high[2] - not_gated_flipflops_normal[2] + not_gated_flipflops_high[2])
-    power_after_clock_gating.append(all_cells_normal_after[3] - clockgated_ff_cells_normal[3] + clockgated_ff_cells_low[3] - clock_gates_normal[3] + clock_gates_high[3] - not_gated_flipflops_normal[3] + not_gated_flipflops_high[3])
+    power_after_clock_gating.append(
+        all_cells_normal_after[0]
+        - clockgated_ff_cells_normal[0]
+        + clockgated_ff_cells_low[0]
+        - clock_gates_normal[0]
+        + clock_gates_high[0]
+        - not_gated_flipflops_normal[0]
+        + not_gated_flipflops_high[0]
+    )
+    power_after_clock_gating.append(
+        all_cells_normal_after[1]
+        - clockgated_ff_cells_normal[1]
+        + clockgated_ff_cells_low[1]
+        - clock_gates_normal[1]
+        + clock_gates_high[1]
+        - not_gated_flipflops_normal[1]
+        + not_gated_flipflops_high[1]
+    )
+    power_after_clock_gating.append(
+        all_cells_normal_after[2]
+        - clockgated_ff_cells_normal[2]
+        + clockgated_ff_cells_low[2]
+        - clock_gates_normal[2]
+        + clock_gates_high[2]
+        - not_gated_flipflops_normal[2]
+        + not_gated_flipflops_high[2]
+    )
+    power_after_clock_gating.append(
+        all_cells_normal_after[3]
+        - clockgated_ff_cells_normal[3]
+        + clockgated_ff_cells_low[3]
+        - clock_gates_normal[3]
+        + clock_gates_high[3]
+        - not_gated_flipflops_normal[3]
+        + not_gated_flipflops_high[3]
+    )
     ####################################################################################################
-    
+
     # generating reports
-    
-    
+
     states.append(
         [
             test[0],
@@ -459,13 +528,20 @@ exit
             str(power_after_clock_gating[2]),
             str(power_after_clock_gating[3]),
             str(power_before_clock_gating[3] - power_after_clock_gating[3]),
-            str(((power_before_clock_gating[3] - power_after_clock_gating[3]) / power_before_clock_gating[3]) * 100) + " %",
+            str(
+                (
+                    (power_before_clock_gating[3] - power_after_clock_gating[3])
+                    / power_before_clock_gating[3]
+                )
+                * 100
+            )
+            + " %",
         ]
     )
-    
+
     ###########################################
-    
-    #states_internal.append(
+
+    # states_internal.append(
     #    [
     #        test[0],
     #        str(all_cells_normal_before[0]),
@@ -485,14 +561,11 @@ exit
     #        str(power_before_clock_gating[3] - power_after_clock_gating[3]),
     #        str(((power_before_clock_gating[3] - power_after_clock_gating[3]) / power_before_clock_gating[3]) * 100) + " %",
     #    ]
-    #)
-    
-    
+    # )
+
     ############################################
 
-
-    
-    #states_switching.append(
+    # states_switching.append(
     #    [
     #        test[0],
     #        str(all_cells_normal_before[1]),
@@ -512,12 +585,11 @@ exit
     #        str(power_before_clock_gating[3] - power_after_clock_gating[3]),
     #        str(((power_before_clock_gating[3] - power_after_clock_gating[3]) / power_before_clock_gating[3]) * 100) + " %",
     #    ]
-    #)
-    
+    # )
+
     ############################################
 
-
-    #states_leakage.append(
+    # states_leakage.append(
     #    [
     #        test[0],
     #        str(all_cells_normal_before[2]),
@@ -537,14 +609,12 @@ exit
     #        str(power_before_clock_gating[3] - power_after_clock_gating[3]),
     #        str(((power_before_clock_gating[3] - power_after_clock_gating[3]) / power_before_clock_gating[3]) * 100) + " %",
     #    ]
-    #)
-    
-    
+    # )
+
     ###########################################
-    
-    
-    #area_reduction = - float(benchmarks_input[j][6]) / float(benchmarks_input[j][4]) * 100
-    #benchmarks.append(
+
+    # area_reduction = - float(benchmarks_input[j][6]) / float(benchmarks_input[j][4]) * 100
+    # benchmarks.append(
     #    [
     #        benchmarks_input[j][0],
     #        benchmarks_input[j][4],
@@ -559,11 +629,11 @@ exit
     #        str(((power_before_clock_gating[3] - power_after_clock_gating[3]) / power_before_clock_gating[3]) * 100) + " %",
     #        str(area_reduction) + " %",
     #    ]
-    #)
-    
+    # )
+
     ############################################
 
-    #power_report_summary.append(
+    # power_report_summary.append(
     #    [
     #        benchmarks_input[j][0],
     #        benchmarks_input[j][4],
@@ -571,8 +641,8 @@ exit
     #        str(((power_before_clock_gating[3] - power_after_clock_gating[3]) / power_before_clock_gating[3]) * 100) + " %",
     #        str(area_reduction) + " %",
     #    ]
-    #)
-    
+    # )
+
     ############################################
 
 
@@ -582,33 +652,33 @@ for row in states:
     writer.writerow(row)
 f.close()
 
-#f = open("./stats/stats_internal_file.csv", "w")
-#writer = csv.writer(f)
-#for row in states_internal:
+# f = open("./stats/stats_internal_file.csv", "w")
+# writer = csv.writer(f)
+# for row in states_internal:
 #    writer.writerow(row)
-#f.close()
+# f.close()
 
-#f = open("./stats/stats_switching_file.csv", "w")
-#writer = csv.writer(f)
-#for row in states_switching:
+# f = open("./stats/stats_switching_file.csv", "w")
+# writer = csv.writer(f)
+# for row in states_switching:
 #    writer.writerow(row)
-#f.close()
+# f.close()
 
-#f = open("./stats/stats_leakage_file.csv", "w")
-#writer = csv.writer(f)
-#for row in states_leakage:
+# f = open("./stats/stats_leakage_file.csv", "w")
+# writer = csv.writer(f)
+# for row in states_leakage:
 #    writer.writerow(row)
-#f.close()
+# f.close()
 
-#f = open("../stats/benchmarks.csv", "w")
-#writer = csv.writer(f)
-#for row in benchmarks:
+# f = open("../stats/benchmarks.csv", "w")
+# writer = csv.writer(f)
+# for row in benchmarks:
 #    writer.writerow(row)
-#f.close()
+# f.close()
 
 
-#f = open("../stats/power_report_summary.csv", "w")
-#writer = csv.writer(f)
-#for row in power_report_summary:
+# f = open("../stats/power_report_summary.csv", "w")
+# writer = csv.writer(f)
+# for row in power_report_summary:
 #    writer.writerow(row)
-#f.close()
+# f.close()
